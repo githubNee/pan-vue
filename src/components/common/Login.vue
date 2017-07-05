bca9f11937a058e5bfbdf26d71b155319f4aed7a
Alina Wang <alinawang1125@gmail.com>
2017-07-05 22:19:36 +0800

    A little bit css change in Login.vue

--- src/components/common/Login.vue
+++ src/components/common/Login.vue
@@ -6,11 +6,16 @@
       </el-menu>
     </template>
     <template v-else>
-      <el-menu theme="dark" class="el-menu-demo" mode="horizontal">
+      <el-menu  class="el-menu-demo" mode="horizontal">
         <el-menu-item index="1"><el-button type="text" @click="open1">登录</el-button></el-menu-item>
       </el-menu>
     </template>
+    <div id="hi">
+      <input type="password" name="code" value="" placeholder="你好啊">
+      <div class="line"></div>
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -44,3 +49,26 @@
     }
   }
 </script>
+<style media="screen">
+  #hi input{
+    background: transparent;
+    border:none;
+    opacity:.6;
+    outline-color: rgba(0,0,0,0);
+    text-align: center;
+    position:absolute;
+    z-index:99;
+    top:10em;
+    right: 0em;
+    line-height: 2em;
+    color: #1eb8d9;
+  }
+  #hi input:hover{
+    opacity: .9;
+  }
+  #hi input:focus{
+    outline:none;
+    border-bottom: 1px solid #1eb8d9;
+  }
+
+</style>
